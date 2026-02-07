b2559c5b633d178bb7da2934d0dc418ef67f2ed7
Doctor0deges <91911461+Doctor0deges@users.noreply.github.com>
2026-02-07 23:26:52 +0100

    Update lock_1_1.sql

--- lock_1_1.sql
+++ lock_1_1.sql
@@ -14,7 +14,7 @@ LOCK TABLES postavy WRITE;
 UNLOCK TABLES;
 
 -- =========================================
--- Zamykani z pohledu dvou uzivatelu - write
+-- Zamykani z pohledu dvou uzivatelu 
 -- =========================================
 
 -- Uzivatel A provede WRITE LOCK:
@@ -30,35 +30,3 @@ select * from Postavy;
 
 -- Uzivatel A odemkne tabulku:
 UNLOCK TABLES;
-
--- =========================================
--- Zamykani z pohledu dvou uzivatelu - read
--- =========================================
-
--- Uzivatel A provede READ LOCK:
-LOCK TABLES postavy READ;
-
--- Uzivatel B se pokusi o UPDATE, ktery se neprovede a ceka:
-UPDATE postavy
-SET Jmeno = 'Novacek'
-WHERE Postava_ID = 1;
-
-select * from Postavy;
--- prikaz ceka, dokud A neodemkne tabulku
-
--- Uzivatel A odemkne tabulku:
-UNLOCK TABLES;
-
--- =========================================
--- Zamceni jednotliveho radku (row-level lock)
--- =========================================
-
-START TRANSACTION;
-
-SELECT *
-FROM postavy
-WHERE Postava_ID = 1
-FOR UPDATE;
--- radek je zamceny pouze pro tento session
-
-COMMIT;
